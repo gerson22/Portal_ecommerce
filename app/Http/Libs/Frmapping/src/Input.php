b56de0485a6ccbd4ef3ff45dfcf66bc791280631
gerson22 <gerson.isaias.rivera@gmail.com>
2017-06-19 15:15:14 -0500

    Libreria frmapping

--- app/Http/Libs/Frmapping/src/Input.php
+++ app/Http/Libs/Frmapping/src/Input.php
@@ -7,11 +7,12 @@ class Input
     
     public static function create($name,$type,$icon,$if_null){
         $required = self::NullToRequerided($if_null);
-        return "<div class='md-form'>
-                       <i class='fa fa-{$icon} prefix'></i>
-                       <input type='{$type}' id='{$name}' name='{$name}' required='{$required}' class='form-control'>
-                       <label for='form3'>{$name}</label>
-                </div>";
+        return '<div class="form-group">
+                    <label for="password" class="col-md-4 control-label">'.$name.'</label>
+                    <div class="col-md-6">
+                        <input id="'.$name.'" type="'.$type.'" required='.$required.' class="form-control" name="'.$name.'">
+                    </div>
+                </div>';
     }
     private static function NullToRequerided($val){
         return  ($val == 'NO') ? 'true' : 'false';
